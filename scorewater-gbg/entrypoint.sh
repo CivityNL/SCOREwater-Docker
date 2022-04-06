11f1aab05897eab17424c4d719228945ef7720a0
Bas Vanmeulebrouk <bas@civity.nl>
2022-04-06 14:25:50 +0200

    adapt to latest version of Java code

--- scorewater-gbg/entrypoint.sh
+++ scorewater-gbg/entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "************************************"
-echo "** PostgreSQL/PostGIS entrypoint  **"
-echo "************************************"
+echo "*********************************"
+echo "** Spring Boot app entrypoint  **"
+echo "*********************************"
 
 set -e  # Stop on error
 set -u  # All variables must be defined
@@ -11,7 +11,7 @@ echo "MQTT URL: ${MQTT_BROKER_URL}"
 echo "MQTT Port: ${MQTT_BROKER_PORT}"
 echo "MQTT Username: ${MQTT_BROKER_USERNAME}"
 echo "MQTT Password: ${MQTT_BROKER_PASSWORD}"
-echo "MQTT Topic: ${MQTT.BROKER.TOPIC}"
+echo "MQTT Topic: ${MQTT_BROKER_TOPIC}"
 
-# Environment variables take precednce over variables defined in application.properties
-java -jar /src/civity/scorewater-minimum-viable-iot-agent/scorewater-gbg-bathing-water/target/scorewater-gbg-bathing-water-1.0-SNAPSHOT.jar --mqtt.broker.url="${MQTT_BROKER_URL}" --mqtt.broker.port="${MQTT_BROKER_PORT}" --mqtt.broker.username="${MQTT_BROKER_USERNAME}" --mqtt.broker.password="${MQTT.BROKER.TOPIC}" --mqtt.broker.topic="${MQTT.BROKER.TOPIC}"
+# Environment variables take precedence over variables defined in application.properties
+java -jar /src/civity/SCOREwater-Java/scorewater-minimum-viable-iot-agent/scorewater-gbg-bathing-water/target/scorewater-gbg-bathing-water-1.0-SNAPSHOT.jar --mqtt.broker.url="${MQTT_BROKER_URL}" --mqtt.broker.port="${MQTT_BROKER_PORT}" --mqtt.broker.username="${MQTT_BROKER_USERNAME}" --mqtt.broker.password="${MQTT_BROKER_PASSWORD}" --mqtt.broker.topic="${MQTT_BROKER_TOPIC}"
