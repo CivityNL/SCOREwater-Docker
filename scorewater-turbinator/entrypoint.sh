b0091bb9647d2b9b573973f32493b2dfee22742c
Bas Vanmeulebrouk <bas@civity.nl>
2022-04-13 12:59:32 +0200

    read database parameters from environment variables provided by compose file

--- scorewater-turbinator/entrypoint.sh
+++ scorewater-turbinator/entrypoint.sh
@@ -20,8 +20,13 @@ echo "MQTT Username: ${MQTT_BROKER_USERNAME}"
 echo "MQTT Password: ${MQTT_BROKER_PASSWORD}"
 echo "MQTT Topic: ${MQTT_BROKER_TOPIC}"
 
+echo "PostgreSQL host: ${PG_HOST}"
+echo "PostgreSQL Port: ${PG_PORT}"
+echo "PostgreSQL Username: ${PG_USER}"
+echo "PostgreSQL Password: ${PG_PASSWORD}"
+echo "PostgreSQL Database: ${PG_DATABASE}"
 
 # Environment variables take precedence over variables defined in application.properties
-java -jar /src/civity/SCOREwater-Java/scorewater-minimum-viable-iot-agent/scorewater-turbinator/target/scorewater-turbinator-1.0-SNAPSHOT.jar --mqtt.broker.url="${MQTT_BROKER_URL}" --mqtt.broker.port="${MQTT_BROKER_PORT}" --mqtt.broker.username="${MQTT_BROKER_USERNAME}" --mqtt.broker.password="${MQTT_BROKER_PASSWORD}" --mqtt.broker.topic="${MQTT_BROKER_TOPIC}"
+java -jar -Xmx128m -Xms64m /src/civity/SCOREwater-Java/scorewater-minimum-viable-iot-agent/scorewater-turbinator/target/scorewater-turbinator-1.0-SNAPSHOT.jar --mqtt.broker.url="${MQTT_BROKER_URL}" --mqtt.broker.port="${MQTT_BROKER_PORT}" --mqtt.broker.username="${MQTT_BROKER_USERNAME}" --mqtt.broker.password="${MQTT_BROKER_PASSWORD}" --mqtt.broker.topic="${MQTT_BROKER_TOPIC}" --spring.datasource.url="jdbc:postgresql://${PG_HOST}:${PG_PORT}/${PG_DATABASE}" --spring.datasource.username="${PG_USER}" --spring.datasource.password="${PG_PASSWORD}"
 
 exit 0
